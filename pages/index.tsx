b46b52aede7ebb889b899ad1d0009f560bef18d1
Cristian Dinu <cristian.dinu@learnfwd.com>
2020-04-02 22:45:26 +0100

    Scores

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import fs from 'fs';
 import path from 'path';
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import matter from 'gray-matter';
 import ReactMarkdown from 'react-markdown';
 
 type Author = 'Tudor' | 'Cristian';
 
-interface PostsOfTheDays {
+interface PostsOfThe {
   [day: number]: {
     [author in Author]: string;
   };
@@ -16,6 +17,10 @@ interface PostsOfTheDays {
 
 interface Challenges {
   [day: number]: string;
+}
+
+interface ScoresOfThe {
+  [day: number]: number;
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
@@ -35,54 +40,88 @@ export const getStaticProps: GetStaticProps = async (context) => {
     }
   }).filter(isNotNull => isNotNull);
 
-  const postsOfTheDays = {};
+  const postsOfThe = {};
+  const scoresOfThe = {};
   const challenges = {};
 
   posts.forEach(({ data, content}) => {
-    const { day, author, challenge } = data;
+    const { day, author, challenge, points } = data;
     if (!day || !author) return;
 
-    postsOfTheDays[day] = postsOfTheDays[day] || {};
-    postsOfTheDays[day][author] = content
+    postsOfThe[day] = postsOfThe[day] || {};
+    postsOfThe[day][author] = content
+
+    scoresOfThe[day] = scoresOfThe[day] || {};
+    scoresOfThe[day][author] = parseInt(points)
+    
     challenges[day] = challenges[day] || challenge;
   })
 
   return {
     props: {
-      postsOfTheDays,
+      postsOfThe,
       challenges,
+      scoresOfThe,
     },
   };
 }
 
-const Index: React.FC<{postsOfTheDays: PostsOfTheDays, challenges: Challenges}> = (props) => {
-  const { postsOfTheDays, challenges } = props;
+interface IndexProps {
+  postsOfThe: PostsOfThe;
+  challenges: Challenges;
+  scoresOfThe: ScoresOfThe;
+}
+
+const Index: React.FC<IndexProps> = (props) => {
+  const { postsOfThe, scoresOfThe, challenges } = props;
+  let currentTudor = 0;
+  let currentCristian = 0;
 
   return (
-    <>
+    <div className="section">
+      <Head>
+        <title>💪 Writing Challenge</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h1 className="title is-1">Writing Challenge!</h1> 
       <div>
-        {Object.keys(postsOfTheDays)
+        {Object.keys(postsOfThe)
           .sort()
           .map(day => (
             <div key={day} style={{ marginBottom: '2em'}}>
-              <h2 className="subtitle">Day {day}</h2>
+              <h2 className="subtitle">
+                Day {day} (
+                  {
+                    scoresOfThe[day].Tudor ? 1 + (currentTudor++) : currentTudor
+                  }
+                  -
+                  {
+                    scoresOfThe[day].Cristian ? 1 + (currentCristian++) : currentCristian
+                  }
+                )
+              </h2>
               <div className="title is-5">Challenge: {challenges[day]}</div>
               <div className="columns">
-                <div className="column has-background-white-ter">
-                  <span className="title is-6">Tudor:</span>
-                  <ReactMarkdown source={postsOfTheDays[day].Tudor} />
+                <div className={scoresOfThe[day].Tudor ? 'column has-background-black-bis': 'column has-background-black-ter'}>
+                  <div className="level">
+                    <div className="is-size-4	 level-item level-left">Tudor</div>
+                    <div className="level-item">{scoresOfThe[day].Tudor ? '+1 ⭐️': null}</div>
+                  </div>
+                  <ReactMarkdown source={postsOfThe[day].Tudor} />
                 </div>
-                <div className="column has-background-white-ter">
-                <span className="title is-6">Cristian:</span>
-                  <ReactMarkdown source={postsOfTheDays[day].Cristian} /> 
+                <div className={scoresOfThe[day].Cristian ? 'column has-background-black-bis': 'column has-background-black-ter'}>
+                  <div className="level">
+                    <div className="is-size-4	 level-item level-left">Cristian</div>
+                    <div className="level-item">{scoresOfThe[day].Cristian ? '+1 ⭐️': null}</div>
+                  </div>
+                  <ReactMarkdown source={postsOfThe[day].Cristian} /> 
                 </div>
               </div>
             </div>
           ))
         }
       </div>
-    </>
+    </div>
   );
 }
 
